108fbced8adbde31d71b66edd681595881b1a0ff
Aron Roos <aron.roos@fau.de>
2026-09-02 11:33:53 +0200

    add flag to save feature vectors

--- utils/parser.py
+++ utils/parser.py
@@ -33,6 +33,7 @@ def get_args():
     parser.add_argument('--start_ckpts', type = str, default=None, help = 'reload used ckpt path')
     parser.add_argument('--ckpts', type = str, default=None, help = 'test used ckpt path')
     parser.add_argument('--val_freq', type = int, default=1, help = 'test freq')
+    parser.add_argument('--save_features', type = int, default=1, help = 'save features')
     parser.add_argument(
         '--resume', 
         action='store_true', 
