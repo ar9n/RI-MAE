895e598f1e8ac9d10bbd4a181dfd34ceaf910c99
Aron Roos <aron.roos@fau.de>
2026-09-14 15:20:39 +0200

    fix: handle missing 'data' key in HDF5 files

--- datasets/io.py
+++ datasets/io.py
@@ -36,4 +36,7 @@ class IO:
     @classmethod
     def _read_h5(cls, file_path):
         f = h5py.File(file_path, 'r')
-        return f['data'][()]
+        if 'data' not in f:
+            return f
+        else:
+            return f['data'][()]
